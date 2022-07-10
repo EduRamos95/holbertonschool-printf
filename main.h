224324d111c083339771e138eda0bf3657f87f59
EduRamos95 <eduramos08.30@gmail.com>
2022-07-10 14:18:30 -0500

    add struct to find a function to specifier

--- main.h
+++ main.h
@@ -5,6 +5,18 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
+typedef struct spec Specifier_t;
+/*struct to find a function to specifier*/
+/**
+  * struct spec - structure to find a specific function
+  * @op: options are preceded by a '%' character.
+  * @f: pointer to function
+  */
+struct spec
+{
+	char *op;
+	int (*f)(va_list);
+};
 
 /* given prototypes */
 int _putchar(char c);
