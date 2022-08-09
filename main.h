a2ad794f068cb46a085ece847b2e6bd022ba730e
EduRamos95 <eduramos08.30@gmail.com>
2022-08-09 00:38:43 -0500

    extra funcs

--- main.h
+++ main.h
@@ -31,4 +31,23 @@ int print_mod(va_list args);
 int print_i(va_list i);
 int print_d(va_list d);
 
+/* Functions_extra1 */
+static int hex_print(char c);
+int print_S(va_list S);
+int print_r(va_list r);
+
+/* Functions_extra2 */
+int print_hex(unsigned int n, unsigned int c);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+
+/* Functions_extra3 */
+int print_R(va_list R);
+
+/* Functions_extra4 */
+int print_u(va_list u);
+int print_o(va_list o);
+int print_b(va_list b);
+
 #endif
